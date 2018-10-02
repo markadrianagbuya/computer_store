84030beb7f4412f06681b47c2b85d836d87d35af
Mark Agbuya <markadrianagbuya@gmail.com>
2018-10-02 21:58:30 +1000

    feat: list first products

--- lib/product.rb
+++ lib/product.rb
@@ -6,4 +6,11 @@ class Product
     self.name = name
     self.price = price
   end
+
+  ALL_PRODUCTS = [
+    Product.new(sku: "ipd", name: "Super iPad", price: 549.99),
+    Product.new(sku: "mbp", name: "MacBook Pro", price: 1399.99),
+    Product.new(sku: "atv", name: "Apple TV", price: 109.50),
+    Product.new(sku: "vga", name: "VGA adapter", price: 30)
+  ]
 end
